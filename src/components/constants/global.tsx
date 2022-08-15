23ecee5835ad1fdf0885d7dbd46822bcbdea0463
aamir-islam <aamirislam38@gmail.com>
2022-08-15 16:23:49 +0530

    AI | change fakeimg size

--- src/components/constants/global.tsx
+++ src/components/constants/global.tsx
@@ -1,6 +1,5 @@
 export const imgUrl = "https://image.tmdb.org/t/p/w500/";
-export const fakeImg = "http://via.placeholder.com/1080x1580/";
-
+export const fakeImg = "https://fakeimg.pl/350x374";
 export const apiKey = "api_key=" + process.env.REACT_APP_API_KEY;
 export const baseUrl = "https://api.themoviedb.org/3";
 export const apiUrl =
