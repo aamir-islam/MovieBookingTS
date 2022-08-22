830f6d1ad185f21acc70b5eca79462f088100dd6
aamir-islam <aamirislam38@gmail.com>
2022-08-22 09:41:55 +0530

    AI | test MovieHome component

--- src/components/MovieList/__test__/MovieHome.test.tsx
+++ src/components/MovieList/__test__/MovieHome.test.tsx
@@ -3,18 +3,23 @@ import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import MovieHome from '../MovieHome'
 
 afterEach(cleanup);
-test("renders Book Tickets heading", () => {
-    render(<MovieHome />)
-    const linkElement = screen.getByText(/Book Tickets/i);
-    expect(linkElement).toBeInTheDocument()
-})
+describe("test for movie card", () => {
+    test('should take a snapshot', () => {
+        const { container } = render(<MovieHome />);
+        expect(container).toMatchSnapshot()
+    })
 
 
-test("Check input value", () => {
-    render(<MovieHome />)
-    const inputValue = screen.getByPlaceholderText<HTMLInputElement>(/Search/i)
-    expect(inputValue).toBeInTheDocument()
-    fireEvent.change(inputValue, { target: { value: "batman" } })
-    expect(inputValue.value).toBe("batman");
+    test("Check input value", () => {
+        render(<MovieHome />)
+        const inputValue = screen.getByPlaceholderText<HTMLInputElement>(/Search/i)
+        expect(inputValue).toBeInTheDocument()
+        fireEvent.change(inputValue, { target: { value: "batman" } })
+        expect(inputValue.value).toBe("batman");
+    })
+
+
+
 })
+
 
