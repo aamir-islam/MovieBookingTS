2df4388bfdfac4e5e774b009ce5d2881e1312a40
aamir-islam <aamirislam38@gmail.com>
2022-08-22 09:40:31 +0530

    AI | test PageNotFound

--- src/components/MovieList/__test__/PNF.test.tsx
+++ src/components/MovieList/__test__/PNF.test.tsx
@@ -3,15 +3,20 @@ import { render, screen } from '@testing-library/react';
 import PNF from '../PNF';
 import pnf from '../../Assets/pageNotFound.png'
 
-test("Heading testing" , ()=>{
-    render(<PNF />)
-    const heading = screen.getByText(/Sorry,there is no result for keyword you searched/i)
-    expect(heading).toBeInTheDocument()
+
+
+describe("testing page not found", () => {
+    test('snapshot for PageNotFound component', () => {
+        const { container } = render(<PNF />);
+        expect(container).toMatchSnapshot()
+    })
+
+    test("Testing for the image attribute", () => {
+        render(<PNF />)
+        const logo = screen.getByRole<HTMLImageElement>('img');
+        expect(logo).toBeInTheDocument()
+        expect(logo).toHaveAttribute('alt', 'pnf');
+        expect(logo).toHaveAttribute('src', pnf);
+    })
 })
-test("Testing for the image" , ()=>{
-    render(<PNF />)
-    const logo = screen.getByRole<HTMLImageElement>('img') ; 
-    expect(logo).toBeInTheDocument()
-    expect(logo).toHaveAttribute('alt', 'pnf');
-    expect(logo).toHaveAttribute('src', pnf);
-})
+
